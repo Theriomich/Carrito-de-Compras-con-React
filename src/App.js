02b00494908ad55b238bb95458ebc71de195247e
Theriomich <theriomich@gmail.com>
2023-03-04 00:38:55 -0300

    Primer comit todo listo por ahora

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import ProductList from "./Components/ProductList";
+import Header from "./Components/Header";
+import { useState } from "react";
+
+function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+  return (
+    <>
+      <Header
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ProductList
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+    </>
+  );
+}
+
+export default App;
